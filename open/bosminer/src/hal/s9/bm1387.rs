3b8890b4e4cbc15409d6aab84e0ffdbf75e37bf8
Jan Čapek <jan.capek@braiins.cz>
2018-09-30 14:28:14 +0200

    rurminer: S9: Implement basic hash chain initialization and chip enumeration
    - factor out IP core specific initialization from s9::init() to
      s9::ip_core_init()
    
    - add s9::enumerate_chips()
    - implement generic method for iterating over all chips (for_all_chips())
    - implement configure_hash_chain()
    - remove any return value from write_to_work_tx_fifo()
    - implement basic variants of read_from_work_rx_fifo(),
      write_to_cmd_tx_fifo(), and read_from_cmd_rx_fifo()
    - implement send_ctl_cmd()
    - implement recv_ctl_cmd_resp() for fetch and unpacking the received response
    - adjust test_hchain_ctl_init to perform also ip_core_init()
    
    - bm1387:
      - CmdHeader:
        - rewrite new() so that it expects complete command length
        - rewrite new_ctl_cmd_header() similarly to use full command length
        - set_chip_address()
      - SetConfigCmd:
        - make header and new() constructor public
      - GetStatusCmd
        - rename GetConfigCmd to GetStatusCmd
        - use nested structure size calculation in new()
      - implement SetChipAddressCmd + unit tests
      - implement InactivateFromChainCmd + unit tests
      - implement GetAddressReg register mapping + chip revision
        enumeration + unit test
      - implement MiscCtrlReg register mapping + unit test
    - cargo.toml - add explicit byteorder crate (1.2.6) dependency and
      adjust cargo.lock accordingly

--- open/bosminer/src/hal/s9/bm1387.rs
+++ open/bosminer/src/hal/s9/bm1387.rs
@@ -1,6 +1,15 @@
 use core;
 use packed_struct::prelude::*;
+
+use std::io;
+use std::mem;
 use std::mem::size_of;
+
+pub const GET_ADDRESS_REG: u8 = 0x00;
+pub const PLL_PARAM_REG: u8 = 0x0c;
+pub const HASH_COUNTING_REG: u8 = 0x14;
+pub const TICKET_MASK_REG: u8 = 0x14;
+pub const MISC_CONTROL_REG: u8 = 0x1c;
 
 #[derive(PackedStruct, Debug)]
 #[packed_struct(size_bytes = "1", bit_numbering = "lsb0")]
@@ -39,17 +48,29 @@ pub struct CmdHeader {
 }
 impl CmdHeader {
     ///
+    /// * `length` - size of the command excluding checksum
     /// * `checksum_size` - Size of checksum needs to be known as it is accounted in the length
     /// field
-    fn new(cmd: Cmd, payload_length: usize, chip_address: u8, checksum_size: usize) -> Self {
+    fn new(cmd: Cmd, length: usize, chip_address: u8, checksum_size: usize) -> Self {
         Self {
             cmd,
-            length: (Self::packed_bytes() + payload_length + checksum_size) as u8,
+            length: (length + checksum_size) as u8,
             chip_address,
         }
     }
-    fn new_ctl_cmd_header(cmd: Cmd, payload_length: usize, chip_address: u8) -> Self {
-        Self::new(cmd, payload_length, chip_address, size_of::<u8>())
+
+    /// Helper builder for control commands
+    /// Control commands CRC5 checksum that fits into 1 byte
+    /// * `length` - length of the command without checksum
+    fn new_ctl_cmd_header(cmd: Cmd, length: usize, chip_address: u8) -> Self {
+        Self::new(cmd, length, chip_address, size_of::<u8>())
+    }
+
+    #[inline]
+    /// Helper method - when streaming multiple commands to eliminate the need to create a new
+    /// command instance every time, we allow modifying the chip address
+    pub fn set_chip_address(&mut self, addr: u8) {
+        self.chip_address = addr;
     }
 }
 
@@ -118,16 +139,15 @@ impl CmdHeader {
 #[packed_struct(endian = "lsb")]
 pub struct SetConfigCmd {
     #[packed_field(element_size_bytes = "3")]
-    header: CmdHeader,
+    pub header: CmdHeader,
     register: u8,
     value: u32,
 }
 impl SetConfigCmd {
-    fn new(chip_address: u8, to_all: bool, register: u8, value: u32) -> SetConfigCmd {
+    pub fn new(chip_address: u8, to_all: bool, register: u8, value: u32) -> Self {
         let cmd = Cmd::new(0x08, to_all);
         // payload consists of 1 byte register address and 4 byte value
-        let header =
-            CmdHeader::new_ctl_cmd_header(cmd, size_of::<u8>() + size_of::<u32>(), chip_address);
+        let header = CmdHeader::new_ctl_cmd_header(cmd, Self::packed_bytes(), chip_address);
         Self {
             header,
             register,
@@ -136,18 +156,135 @@ impl SetConfigCmd {
     }
 }
 
-struct GetConfigCmd {
+#[derive(PackedStruct, Debug)]
+#[packed_struct(endian = "lsb")]
+pub struct GetStatusCmd {
+    #[packed_field(element_size_bytes = "3")]
     header: CmdHeader,
     register: u8,
 }
-impl GetConfigCmd {
-    fn new(chip_address: u8, to_all: bool, register: u8) -> GetConfigCmd {
+
+impl GetStatusCmd {
+    pub fn new(chip_address: u8, to_all: bool, register: u8) -> Self {
         let cmd = Cmd::new(0x04, to_all);
-        let header = CmdHeader::new_ctl_cmd_header(cmd, size_of::<u8>(), chip_address);
+        let header = CmdHeader::new_ctl_cmd_header(cmd, Self::packed_bytes(), chip_address);
         Self { header, register }
     }
 }
 
+#[derive(PackedStruct, Debug)]
+#[packed_struct(endian = "lsb")]
+pub struct SetChipAddressCmd {
+    #[packed_field(element_size_bytes = "3")]
+    pub header: CmdHeader,
+    _reserved: u8,
+}
+
+impl SetChipAddressCmd {
+    pub fn new(chip_address: u8) -> Self {
+        let cmd = Cmd::new(0x01, false);
+        let header = CmdHeader::new_ctl_cmd_header(cmd, Self::packed_bytes(), chip_address);
+        Self {
+            header,
+            _reserved: 0,
+        }
+    }
+}
+
+#[derive(PackedStruct, Debug)]
+#[packed_struct(endian = "lsb")]
+pub struct InactivateFromChainCmd {
+    #[packed_field(element_size_bytes = "3")]
+    header: CmdHeader,
+    _reserved: u8,
+}
+
+impl InactivateFromChainCmd {
+    pub fn new() -> Self {
+        let cmd = Cmd::new(0x05, true);
+        let header = CmdHeader::new_ctl_cmd_header(cmd, Self::packed_bytes(), 0);
+        Self {
+            header,
+            _reserved: 0,
+        }
+    }
+}
+
+#[derive(PackedStruct, Debug)]
+#[packed_struct(endian = "lsb")]
+pub struct GetAddressReg {
+    #[packed_field(endian = "msb", ty = "enum", element_size_bytes = "2")]
+    pub chip_rev: ChipRev,
+    _reserved1: u8,
+    pub addr: u8,
+}
+
+#[derive(PrimitiveEnum_u16, Clone, Copy, Debug, PartialEq)]
+/// Command types
+pub enum ChipRev {
+    /// Control command for the chip
+    Bm1387 = 0x1387,
+}
+//0x40, 0x20, 0x9a, 0x80
+//#[packed_struct(size_bytes = "1", bit_numbering = "lsb0")]
+///// Control or work command layout
+//pub struct Cmd {
+//    #[packed_field(bits = "0:3")]
+//    code: Integer<u8, packed_bits::Bits4>,
+//    #[packed_field(bits = "4")]
+
+/// Core register that configures the most important aspects of the mining chip like:
+///
+/// - baud rate/communication speed
+/// - multi-midstate processing (AsicBoost)
+///
+/// All the fields below have been identified in bmminer-mix sources. Meaning of some of them may
+/// still be a bit unclear.
+///
+/// TODO: research set_baud_with_addr() in bmminer-mix as there seems to be some magic setting
+/// I2C interface of the chip or something like that
+#[derive(PackedStruct, Debug)]
+#[packed_struct(bit_numbering = "lsb0", size_bytes = "4", endian = "lsb")]
+pub struct MiscCtrlReg {
+    /// Exact meaning of this field is unknown, when setting baud rate, it is 0, when
+    /// initializing the chain it is 1
+    /// bit 6
+    #[packed_field(bits = "30")]
+    pub not_set_baud: bool,
+
+    /// Invert clock pin -> used on S9's
+    /// bit 13
+    #[packed_field(bits = "21")]
+    pub inv_clock: bool,
+
+    /// baudrate divisor - maximum divisor is 26. To calculate the divisor:
+    /// baud_div = min(OSC/8*baud - 1, 26)
+    /// Oscillator frequency is 25 MHz
+    /// bit 20:16
+    #[packed_field(bits = "12:8")]
+    pub baud_div: Integer<u8, packed_bits::Bits5>,
+
+    /// This field most probably causes the core to
+    /// bit 23
+    #[packed_field(bits = "15")]
+    pub gate_block: bool,
+
+    /// Enable multi midstate processing = "AsicBoost"
+    /// bit 31
+    #[packed_field(bits = "7")]
+    pub mmen: bool,
+}
+
+impl MiscCtrlReg {
+    pub fn to_u32(&self) -> u32 {
+        let mut reg_bytes = self.pack();
+
+        let value = unsafe { mem::transmute(reg_bytes) };
+        value
+    }
+}
+
+/////
 //impl<T> CtlCmd<T> {
 //    fn new_for_fpga(cmd: Cmd, chip_address: u8, payload: T) -> FpgaCmd<Self, u32> {
 //        let cmd_with_payload = Self {
@@ -243,17 +380,132 @@ mod test {
     /// 0x21026800 that corresponds to
     /// and verifies correct serialization
     fn build_set_config_cmd() {
-        let cmd = SetConfigCmd::new(0x00, false, 0x0c, 0x21026800);
-        let expected_cmd_with_padding = [0x48u8, 0x09, 0x00, 0x0c, 0x00, 0x68, 0x02, 0x21];
+        let cmd = SetConfigCmd::new(0x00, false, PLL_PARAM_REG, 0x21026800);
+        let expected_cmd_with_padding = [0x48u8, 0x09, 0x00, PLL_PARAM_REG, 0x00, 0x68, 0x02, 0x21];
         //        let expected_cmd_with_padding = u8_as_fpga_slice(&expected_cmd_with_padding);
         let cmd_bytes = cmd.pack();
         assert_eq!(
             cmd_bytes, expected_cmd_with_padding,
-            "Incorrectly composed command:{:#04x?} sliced view: {:#010x?} expected view: \
-             {:#010x?}",
+            "Incorrectly composed command:{:#04x?} sliced view: {:#04x?} expected view: \
+             {:#04x?}",
             cmd, cmd_bytes, expected_cmd_with_padding
         );
     }
+
+    #[test]
+    /// Builds a get status command to read chip address of all chips
+    fn build_get_status_cmd() {
+        let cmd = GetStatusCmd::new(0x00, true, GET_ADDRESS_REG);
+        let expected_cmd_with_padding = [0x54u8, 0x05, 0x00, 0x00];
+
+        let cmd_bytes = cmd.pack();
+        assert_eq!(
+            cmd_bytes, expected_cmd_with_padding,
+            "Incorrectly composed command:{:#04x?} sliced view: {:#04x?} expected view: \
+             {:#04x?}",
+            cmd, cmd_bytes, expected_cmd_with_padding
+        );
+    }
+
+    #[test]
+    fn build_inactivate_from_chain_cmd() {
+        let cmd = InactivateFromChainCmd::new();
+        let expected_cmd_with_padding = [0x55u8, 0x05, 0x00, 0x00];
+
+        let cmd_bytes = cmd.pack();
+        assert_eq!(
+            cmd_bytes, expected_cmd_with_padding,
+            "Incorrectly composed command:{:#04x?} sliced view: {:#04x?} expected view: \
+             {:#04x?}",
+            cmd, cmd_bytes, expected_cmd_with_padding
+        );
+    }
+
+    #[test]
+    fn build_set_chip_address_cmd() {
+        let cmd = SetChipAddressCmd::new(0x04);
+        let expected_cmd_with_padding = [0x41u8, 0x05, 0x04, 0x00];
+
+        let cmd_bytes = cmd.pack();
+        assert_eq!(
+            cmd_bytes, expected_cmd_with_padding,
+            "Incorrectly composed command:{:#04x?} sliced view: {:#04x?} expected view: \
+             {:#04x?}",
+            cmd, cmd_bytes, expected_cmd_with_padding
+        );
+    }
+
+    #[test]
+    fn build_chip_addr_reg() {
+        let reg = GetAddressReg {
+            chip_rev: ChipRev::Bm1387,
+            _reserved1: 0x90,
+            addr: 0x00,
+        };
+        let expected_reg = [0x13u8, 0x87, 0x90, 0x00];
+
+        let reg_bytes = reg.pack();
+        assert_eq!(
+            reg_bytes, expected_reg,
+            "Incorrectly composed register:{:#04x?} sliced view: {:#04x?} expected view: \
+             {:#04x?}",
+            reg, reg_bytes, expected_reg
+        );
+    }
+
+    #[test]
+    fn test_broken_chip_addr_value() {
+        // intentionally specify incorrect/unsupported chip version
+        let broken_reg_bytes = [0x13u8, 0x86, 0x90, 0x04];
+        let reg = GetAddressReg::unpack_from_slice(&broken_reg_bytes);
+        assert!(
+            reg.is_err(),
+            "Unpacking should have failed due to incompatible chip version \
+             parsed: {:?}, sliced view: {:#04x?}",
+            reg,
+            broken_reg_bytes
+        );
+    }
+
+    #[test]
+    fn build_misc_control_reg() {
+        let reg = MiscCtrlReg {
+            not_set_baud: true,
+            inv_clock: true,
+            baud_div: 26.into(),
+            gate_block: true,
+            mmen: true,
+        };
+        let expected_reg = [0x40u8, 0x20, 0x9a, 0x80];
+
+        let reg_bytes = reg.pack();
+
+        assert_eq!(
+            reg_bytes, expected_reg,
+            "Incorrectly composed register:{:#04x?} sliced view: {:#04x?} expected view: \
+             {:#04x?}",
+            reg, reg_bytes, expected_reg
+        );
+    }
+
+    #[test]
+    fn test_misc_control_reg_to_u32() {
+        let reg = MiscCtrlReg {
+            not_set_baud: true,
+            inv_clock: true,
+            baud_div: 26.into(),
+            gate_block: true,
+            mmen: true,
+        };
+        let expected_reg_value = 0x809a2040u32;
+        let reg_value = reg.to_u32();
+        assert_eq!(
+            reg_value, expected_reg_value,
+            "Misc Control Register 32-bit value  doesn't match: {} V:{:#010x} E:{:#010x}",
+            reg, reg_value, expected_reg_value
+        );
+    }
+
 }
 
 //impl DeactivateChainCmd {
--- open/bosminer/src/hal/s9/mod.rs
+++ open/bosminer/src/hal/s9/mod.rs
@@ -1,3 +1,4 @@
+extern crate byteorder;
 extern crate libc;
 extern crate nix;
 extern crate s9_io;
@@ -10,12 +11,24 @@ use std::io;
 use std::mem::size_of;
 use std::os::unix::fs::OpenOptionsExt;
 use std::os::unix::io::AsRawFd;
+// TODO: remove thread specific components
+use std::thread;
+use std::time::Duration;
 
 use uint;
 
+use self::byteorder::{ByteOrder, LittleEndian};
+use packed_struct::{PackedStruct, PackedStructSlice};
 use self::s9_io::hchainio0;
 
 mod bm1387;
+
+/// Timing constants
+const INACTIVATE_FROM_CHAIN_DELAY_MS: u64 = 100;
+
+/// Maximum number of chips is limitted by the fact that there is only 8-bit address field and
+/// addresses to the chips need to be assigned with step of 4 (e.g. 0, 4, 8, etc.)
+const MAX_CHIPS_ON_CHAIN: usize = 64;
 
 /// Hash Chain Controller provides abstraction of the FPGA interface for operating hashing boards.
 /// It is the user-space driver for the
@@ -28,6 +41,7 @@ mod bm1387;
 pub struct HChainCtl<'a> {
     hash_chain_ios: [&'a hchainio0::RegisterBlock; 2],
     work_id: u16,
+    chip_count: usize,
 }
 
 impl<'a> HChainCtl<'a> {
@@ -56,11 +70,12 @@ impl<'a> HChainCtl<'a> {
         Result::Ok(Self {
             hash_chain_ios: [hash_chain_io, hash_chain_io],
             work_id: 0,
+            chip_count: 0,
         })
     }
 
-    // TODO handle error
-    pub fn init(&self) {
+    /// Helper method that initializes the FPGA IP core
+    fn ip_core_init(&self) -> Result<(), io::Error> {
         // Disable/enable performs reset of the entire FPGA IP core
         self.disable();
         self.enable();
@@ -69,6 +84,105 @@ impl<'a> HChainCtl<'a> {
         // TODO consolidate hardcoded constant - calculate time constant based on PLL settings etc.
         self.set_work_time(50000);
         self.set_midstate_count(hchainio0::ctrl_reg::MIDSTATE_CNTW::ONE);
+
+        Ok(())
+    }
+
+    /// Initializes the chips on the chain
+    pub fn init(&mut self) -> Result<(), io::Error> {
+        self.ip_core_init()?;
+
+        self.enumerate_chips()?;
+        println!("Discovered {} chips", self.chip_count);
+
+        // set PLL
+        self.set_pll()?;
+
+        // enable hashing chain
+        self.configure_hash_chain()?;
+
+        Ok(())
+    }
+
+    /// Detects the number of chips on the hashing chain and assigns an address to each chip
+    fn enumerate_chips(&mut self) -> Result<(), io::Error> {
+        // Enumerate all chips (broadcast read address register request)
+        let get_addr_cmd = bm1387::GetStatusCmd::new(0, true, bm1387::GET_ADDRESS_REG).pack();
+        self.send_ctl_cmd(&get_addr_cmd);
+        self.chip_count = 0;
+        while let Ok(addr_reg) = self.recv_ctl_cmd_resp::<bm1387::GetAddressReg>() {
+            assert_ne!(
+                addr_reg.chip_rev,
+                bm1387::ChipRev::Bm1387,
+                "Unexpected chip revision"
+            );
+            self.chip_count += 1;
+        }
+        if self.chip_count >= MAX_CHIPS_ON_CHAIN {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("Detected {} chips, expected less than 256 chips on 1 chain. Possibly a hardware issue?",
+                    self.chip_count
+                ),
+            ));
+        }
+        // Set all chips to be offline before address assignment. This is important so that each
+        // chip after initially accepting the address will pass on further addresses down the chain
+        let inactivate_from_chain_cmd = bm1387::InactivateFromChainCmd::new().pack();
+        // make sure all chips receive inactivation request
+        for _ in 0..3 {
+            self.send_ctl_cmd(&inactivate_from_chain_cmd);
+            thread::sleep(Duration::from_millis(INACTIVATE_FROM_CHAIN_DELAY_MS));
+        }
+
+        // Assign address to each chip
+        self.for_all_chips(|addr| {
+            let cmd = bm1387::SetChipAddressCmd::new(addr);
+            self.send_ctl_cmd(&cmd.pack());
+            Ok(())
+        })?;
+
+        Ok(())
+    }
+
+    /// Helper method that applies a function to all detected chips on the chain
+    fn for_all_chips<F, R>(&self, f: F) -> Result<R, io::Error>
+    where
+        F: Fn(u8) -> Result<R, io::Error>,
+    {
+        let mut result: Result<R, io::Error> =
+            Err(io::Error::new(io::ErrorKind::Other, "no chips to iterate"));
+        for addr in (0..self.chip_count * 4).step_by(4) {
+            // the enumeration takes care that address always fits into 8 bits.
+            // Therefore, we can truncate the bits here.
+            result = Ok(f(addr as u8)?);
+        }
+        // Result of last iteration
+        result
+    }
+
+    /// Loads PLL register with a starting value
+    fn set_pll(&self) -> Result<(), io::Error> {
+        self.for_all_chips(|addr| {
+            let cmd = bm1387::SetConfigCmd::new(addr, false, bm1387::PLL_PARAM_REG, 0x21026800);
+            self.send_ctl_cmd(&cmd.pack());
+            Ok(())
+        })
+    }
+
+    /// TODO: consolidate hardcoded baudrate to 115200
+    fn configure_hash_chain(&self) -> Result<(), io::Error> {
+        let ctl_reg = bm1387::MiscCtrlReg {
+            not_set_baud: true,
+            inv_clock: true,
+            baud_div: 26.into(),
+            gate_block: true,
+            mmen: true,
+        };
+        let ctl_reg_u32 = ctl_reg.to_u32();
+        let cmd = bm1387::SetConfigCmd::new(0, true, bm1387::MISC_CONTROL_REG, ctl_reg_u32);
+        self.send_ctl_cmd(&cmd.pack());
+        Ok(())
     }
 
     fn enable(&self) {
@@ -121,22 +235,77 @@ impl<'a> HChainCtl<'a> {
     #[inline]
     /// TODO: implement error handling/make interface ready for ASYNC execution
     /// Writes single word into a TX fifo
-    fn write_to_work_tx_fifo(&self, item: u32) -> bool {
+    fn write_to_work_tx_fifo(&self, item: u32) {
         let hash_chain_io = self.hash_chain_ios[0];
-        let may_write = !hash_chain_io.stat_reg.read().cmd_tx_full().bit();
-        if may_write == true {
-            hash_chain_io
-                .work_tx_fifo
-                .write(|w| unsafe { w.bits(item) });
-        }
-        return may_write;
+        while !hash_chain_io.stat_reg.read().work_tx_full().bit() {}
+        hash_chain_io
+            .work_tx_fifo
+            .write(|w| unsafe { w.bits(item) });
     }
 
     #[inline]
-    fn next_work_id(&mut self) -> u32 {
-        let retval = self.work_id as u32;
-        self.work_id += 1;
-        retval
+    /// TODO get rid of busy waiting, prepare for non-blocking API
+    fn read_from_work_rx_fifo(&self) -> u32 {
+        let hash_chain_io = self.hash_chain_ios[0];
+        while hash_chain_io.stat_reg.read().work_rx_empty().bit() {}
+        hash_chain_io.work_rx_fifo.read().bits()
+    }
+
+    #[inline]
+    /// TODO get rid of busy waiting, prepare for non-blocking API
+    fn write_to_cmd_tx_fifo(&self, item: u32) {
+        let hash_chain_io = self.hash_chain_ios[0];
+        while hash_chain_io.stat_reg.read().cmd_tx_full().bit() {}
+        hash_chain_io.cmd_tx_fifo.write(|w| unsafe { w.bits(item) });
+    }
+
+    #[inline]
+    fn read_from_cmd_rx_fifo(&self) -> Result<u32, io::Error> {
+        let hash_chain_io = self.hash_chain_ios[0];
+        if hash_chain_io.stat_reg.read().cmd_rx_empty().bit() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Command RX fifo empty",
+            ));
+        }
+        Ok(hash_chain_io.cmd_rx_fifo.read().bits())
+    }
+
+    /// Serializes command into 32-bit words and submits it to the command TX FIFO
+    ///
+    fn send_ctl_cmd(&self, cmd: &[u8]) {
+        // invariant required by the IP core
+        assert_eq!(
+            cmd.len() & 0x3,
+            0,
+            "Control command length not aligned to 4 byte boundary!"
+        );
+        for chunk in cmd.chunks(4) {
+            self.write_to_cmd_tx_fifo(LittleEndian::read_u32(chunk));
+        }
+    }
+
+    /// # TODO
+    ///
+    /// # Errors
+    ///
+    fn recv_ctl_cmd_resp<T: PackedStructSlice>(&self) -> Result<T, io::Error> {
+        let mut cmd_resp = [0u8; 8];
+        // fetch command response from IP core's fifo
+        for cmd in cmd_resp.chunks_mut(4) {
+            let resp_word = self.read_from_cmd_rx_fifo()?;
+            LittleEndian::write_u32(cmd, resp_word);
+        }
+        // build the response instance
+        T::unpack_from_slice(&cmd_resp).map_err(|e| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                format!(
+                    "Control command unpacking error! {:?} {:#04x?}",
+                    e, cmd_resp
+                ),
+            )
+        })
     }
 }
 
@@ -188,8 +357,12 @@ mod test {
     fn test_hchain_ctl_init() {
         let h_chain_ctl = HChainCtl::new().unwrap();
 
-        h_chain_ctl.init();
+        assert!(
+            h_chain_ctl.ip_core_init().is_ok(),
+            "Failed to initialize IP core"
+        );
 
+        // verify sane register values
         assert_eq!(
             h_chain_ctl.hash_chain_ios[0].work_time.read().bits(),
             50000,
