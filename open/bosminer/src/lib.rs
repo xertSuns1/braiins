2d33a7ac840910aa7a2f93f949374eb983dd8af5
Jan Čapek <jan.capek@braiins.cz>
2018-10-01 11:53:57 +0200

    rurminer: Convert the rminer crate into a library for easier integration testing

---
+++ open/bosminer/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate core;
+#[macro_use]
+extern crate packed_struct_codegen;
+extern crate packed_struct;
+extern crate s9_io;
+extern crate uint;
+
+pub mod hal;
--- open/bosminer/src/main.rs
+++ open/bosminer/src/main.rs
@@ -1,15 +1,7 @@
-//#[macro_use]
-//extern crate bitfield;
-extern crate core;
-#[macro_use]
-extern crate packed_struct_codegen;
-extern crate packed_struct;
-extern crate uint;
-
-mod hal;
+extern crate rminer;
 
 fn main() {
-    println!("BraiinsOS and what's in your miner?");
+    println!("BraiinsOS. And what's in your miner?");
     println!("rminer is coming soon, run tests for now by executing:");
     println!("cargo test");
 }
