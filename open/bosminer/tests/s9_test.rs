ff6f432acb3de46c321c59f76e4f91fd171ba7ec
Jan Čapek <jan.capek@braiins.cz>
2018-12-27 12:38:56 +0100

    rurminer: s9: Refactor work submission integration test
    - move mining work instantiation to prepare_test_work
    
    - factor out send/receive test work to send_and_receive_test_workloads()
    
    - implement 2 basic test steps and document in the sources of how the
      test workloads are structured and that we expect to get back from
      the hash board

--- open/bosminer/tests/s9_test.rs
+++ open/bosminer/tests/s9_test.rs
@@ -10,6 +10,58 @@ use rminer::hal::HardwareCtl;
 
 use linux_embedded_hal::I2cdev;
 
+use std::thread;
+use std::time::Duration;
+
+/// * `_i` - unique identifier that makes the work distinct
+fn prepare_test_work(_i: usize) -> hal::MiningWork {
+    hal::MiningWork {
+        version: 0,
+        extranonce_2: 0,
+        midstates: vec![uint::U256([0, 0, 0, 0])],
+        merkel_root_lsw: 0xffff_ffff,
+        nbits: 0xffff_ffff,
+        ntime: 0xffff_ffff,
+        //            version: 0,
+        //            extranonce_2: 0,
+        //            midstates: vec![uint::U256([v, 2, 3, 4])],
+        //            merkel_root_lsw: 0xdeadbeef,
+        //            nbits: 0x1a44b9f2,
+        //            ntime: 0x4dd7f5c7,
+    }
+}
+
+/// * `work_start` - beginning of the unique test work range
+/// * `end_start` - end of the unique test work range (excluded)
+fn send_and_receive_test_workloads(
+    h_chain_ctl: &mut hal::s9::HChainCtl,
+    work_start: usize,
+    work_end: usize,
+    expected_result_count: usize,
+) {
+    let mut work_result_count = 0usize;
+
+    for i in work_start..work_end {
+        let test_work = prepare_test_work(i);
+        let work_id = h_chain_ctl.send_work(&test_work).unwrap();
+        // wait until the work is physically sent out
+        thread::sleep(Duration::from_millis(10));
+        while let Some(work_result) = h_chain_ctl.recv_work_result().unwrap() {
+            println!("Iteration:{}\n{:#010x?}", i, work_result);
+            assert_eq!(
+                work_id,
+                h_chain_ctl.get_work_id_from_result(&work_result),
+                "Unexpected work ID detected in returned mining result"
+            );
+            work_result_count += 1;
+        }
+    }
+    assert_eq!(
+        work_result_count, expected_result_count,
+        "Unexpected number of work results"
+    )
+}
+
 #[test]
 fn test_work_generation() {
     let gpio_mgr = gpio::ControlPinManager::new();
@@ -22,34 +74,11 @@ fn test_work_generation() {
     ).unwrap();
 
     h_chain_ctl.init().unwrap();
-    let mut result_count = 0usize;
 
-    for v in 0..6 {
-        let test_work = hal::MiningWork {
-            version: 0,
-            extranonce_2: 0,
-            midstates: vec![uint::U256([0, 0, 0, 0])],
-            merkel_root_lsw: 0xffff_ffff,
-            nbits: 0xffff_ffff,
-            ntime: 0xffff_ffff,
-            //            version: 0,
-            //            extranonce_2: 0,
-            //            midstates: vec![uint::U256([v, 2, 3, 4])]
-            //            merkel_root_lsw: 0xdeadbeef,
-            //            nbits: 0x1a44b9f2,
-            //            ntime: 0x4dd7f5c7,
-        };
-
-        h_chain_ctl.send_work(&test_work);
-
-        match h_chain_ctl.recv_work_result().unwrap() {
-            Some(work_result) => {
-                println!("{:#010x?}", work_result);
-                result_count += 1;
-            }
-            None => println!("No new result"),
-        }
-    }
-
-    while true {}
+    // the first 3 work loads don't produce any results, these are merely to initialize the input
+    // queue of each hashing chip
+    send_and_receive_test_workloads(&mut h_chain_ctl, 0, 3, 0);
+    // submit 2 more workloads, these are expected to yield the same results from every single chip
+    let expected_result_count = 2 * h_chain_ctl.get_chip_count();
+    send_and_receive_test_workloads(&mut h_chain_ctl, 3, 5, expected_result_count);
 }
