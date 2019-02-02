02a7f4e2c82da9ff79a84e9954c3fd0e7748e5b1
Jan Čapek <jan.capek@braiins.cz>
2019-02-03 00:49:29 +0100

    rurminer: Add support for logging using 'slog'
    - implement central logger instance initialization in a new module 'misc'
    - adjust Cargo.toml
    - add auto-adjusted Cargo.lock
    - publish the 'misc' module in the root package

--- open/bosminer/src/lib.rs
+++ open/bosminer/src/lib.rs
@@ -3,10 +3,15 @@ extern crate core;
 extern crate packed_struct_codegen;
 extern crate byteorder;
 extern crate embedded_hal;
+extern crate lazy_static;
 extern crate linux_embedded_hal;
 extern crate packed_struct;
 extern crate s9_io;
+extern crate slog;
+extern crate slog_async;
+extern crate slog_term;
 extern crate sysfs_gpio;
 extern crate uint;
 
 pub mod hal;
+pub mod misc;
