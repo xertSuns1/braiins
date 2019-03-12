320968da05cdca518eab2e43c4e69f34fcf2f12f
Libor Vašíček <libor.vasicek@braiins.com>
2019-03-12 19:34:13 +0100

    rurminer: Switch project to rust 2018 edition
    - force edition to 2018
    - remove 'extern' crates from sources
    - rename 'mod.rs' to support path clarity
    - replace #[macro_use] with 'use' syntax

--- open/bosminer/src/lib.rs
+++ open/bosminer/src/lib.rs
@@ -1,17 +1,2 @@
-extern crate core;
-#[macro_use]
-extern crate packed_struct_codegen;
-extern crate byteorder;
-extern crate embedded_hal;
-extern crate lazy_static;
-extern crate linux_embedded_hal;
-extern crate packed_struct;
-extern crate s9_io;
-extern crate slog;
-extern crate slog_async;
-extern crate slog_term;
-extern crate sysfs_gpio;
-extern crate uint;
-
 pub mod hal;
 pub mod misc;
--- open/bosminer/src/main.rs
+++ open/bosminer/src/main.rs
@@ -1,5 +1,3 @@
-extern crate rminer;
-
 fn main() {
     println!("BraiinsOS. And what's in your miner?");
     println!("rminer is coming soon, run tests for now by executing:");
