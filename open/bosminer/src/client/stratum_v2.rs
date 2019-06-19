bc1b1d437161a4e6822b5d0458641e88b9b846a1
Libor Vašíček <libor.vasicek@braiins.com>
2019-06-19 17:46:57 +0200

    rurminer: Create StratumEventHandler
    - add structure 'StratumEventHandler' which will be used for handling
      incoming notifications from stratum server

--- open/bosminer/src/client/stratum_v2.rs
+++ open/bosminer/src/client/stratum_v2.rs
@@ -1,9 +1,13 @@
 use crate::hal;
 use crate::workhub;
 
+use futures::future::Future;
+
+use stratum::v2::messages::{NewMiningJob, SetNewPrevhash, SetTarget};
+use stratum::v2::{V2Handler, V2Protocol};
+use wire::Message;
+
 use bitcoin_hashes::{sha256d::Hash, Hash as HashTrait};
-use stratum::v2::messages::{NewMiningJob, SetNewPrevhash};
-use stratum::v2::V2Handler;
 
 // TODO: move it to the stratum crate
 const VERSION_MASK: u32 = 0x1fffe000;
@@ -68,6 +72,28 @@ impl hal::BitcoinJob for StratumJob {
     }
 }
 
-struct StratumClient;
+struct StratumEventHandler {
+    job_sender: workhub::JobSender,
+}
 
-impl StratumClient {}
+impl StratumEventHandler {
+    pub fn new(job_sender: workhub::JobSender) -> Self {
+        Self { job_sender }
+    }
+}
+
+impl V2Handler for StratumEventHandler {
+    fn visit_new_mining_job(&mut self, _msg: &Message<V2Protocol>, _payload: &NewMiningJob) {}
+    fn visit_set_new_prevhash(&mut self, _msg: &Message<V2Protocol>, _payload: &SetNewPrevhash) {}
+    fn visit_set_target(&mut self, _msg: &Message<V2Protocol>, _payload: &SetTarget) {}
+}
+
+pub async fn run(stratum_addr: String, job_solver: workhub::JobSolver) {
+    let (job_sender, mut job_solution) = job_solver.split();
+
+    // TODO: run event handler in separate task
+    let event_handler = StratumEventHandler::new(job_sender);
+
+    // TODO: send solutions via RPC back to stratum server
+    while let Some(_x) = await!(job_solution.receive()) {}
+}
