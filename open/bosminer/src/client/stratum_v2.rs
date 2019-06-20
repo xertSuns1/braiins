742823a90daab278656e3ceb5ca4621712b00c7f
Libor Vašíček <libor.vasicek@braiins.com>
2019-06-20 12:48:49 +0200

    rurminer: Create new job from stratum V2
    - combine 'NewMiningJob' and 'SetNewPrevhash' to create new job which
      is immediately send to backend to start working on it

--- open/bosminer/src/client/stratum_v2.rs
+++ open/bosminer/src/client/stratum_v2.rs
@@ -3,11 +3,14 @@ use crate::workhub;
 
 use futures::future::Future;
 
+use std::sync::Arc;
+
 use stratum::v2::messages::{NewMiningJob, SetNewPrevhash, SetTarget, SubmitShares};
 use stratum::v2::{V2Handler, V2Protocol};
 use wire::Message;
 
 use bitcoin_hashes::{sha256d::Hash, Hash as HashTrait};
+use std::collections::HashMap;
 
 // TODO: move it to the stratum crate
 const VERSION_MASK: u32 = 0x1fffe000;
@@ -26,7 +29,7 @@ struct StratumJob {
 }
 
 impl StratumJob {
-    pub fn new(job_msg: NewMiningJob, prevhash_msg: SetNewPrevhash) -> Self {
+    pub fn new(job_msg: &NewMiningJob, prevhash_msg: &SetNewPrevhash) -> Self {
         assert_eq!(job_msg.block_height, prevhash_msg.block_height);
         Self {
             id: job_msg.job_id,
@@ -74,25 +77,48 @@ impl hal::BitcoinJob for StratumJob {
 
 struct StratumEventHandler {
     job_sender: workhub::JobSender,
+    new_jobs: HashMap<u32, NewMiningJob>,
 }
 
 impl StratumEventHandler {
     pub fn new(job_sender: workhub::JobSender) -> Self {
-        Self { job_sender }
+        Self {
+            job_sender,
+            new_jobs: Default::default(),
+        }
     }
 }
 
 impl V2Handler for StratumEventHandler {
-    fn visit_new_mining_job(&mut self, _msg: &Message<V2Protocol>, job_mgs: &NewMiningJob) {}
+    fn visit_new_mining_job(&mut self, _msg: &Message<V2Protocol>, job_mgs: &NewMiningJob) {
+        // insert new job or update old one with the same block height
+        self.new_jobs.insert(job_mgs.block_height, job_mgs.clone());
+        // TODO: close connection when maximal capacity of new jobs has been reached
+    }
 
     fn visit_set_new_prevhash(
         &mut self,
         _msg: &Message<V2Protocol>,
         prevhash_msg: &SetNewPrevhash,
     ) {
+        let current_block_height = prevhash_msg.block_height;
+        // find a job with the same block height as provided in previous hash
+        if let Some((_, job_msg)) = self.new_jobs.remove_entry(&current_block_height) {
+            let job = StratumJob::new(&job_msg, prevhash_msg);
+            self.job_sender.send(Arc::new(job));
+        } else {
+            // TODO: close connection when any job with provided block height hasn't been found
+            panic!("cannot find any job for current block height");
+        }
+
+        // remove all jobs with lower block height
+        self.new_jobs
+            .retain(move |&block_height, _| block_height >= current_block_height);
     }
 
-    fn visit_set_target(&mut self, _msg: &Message<V2Protocol>, target_msg: &SetTarget) {}
+    fn visit_set_target(&mut self, _msg: &Message<V2Protocol>, target_msg: &SetTarget) {
+        // TODO: use job_sender to update new target
+    }
 }
 
 struct StratumSolutionHandler {
