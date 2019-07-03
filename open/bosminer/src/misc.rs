68d9de63e34955596ceb6646a685d34d75924932
Jakub Horak <jakub.horak@braiins.com>
2019-07-03 13:12:25 +0200

    rurminer: Add support for envlogger
    This enables selective per-module debug levels configurable via
    RUST_LOG environment variable.

--- open/bosminer/src/misc.rs
+++ open/bosminer/src/misc.rs
@@ -1,18 +1,27 @@
+use std::env;
 use std::fs::OpenOptions;
 
 use lazy_static::lazy_static;
 use slog::{o, Drain, Duplicate, Level, LevelFilter, Logger};
 use slog_async;
+use slog_envlogger;
 use slog_term;
 
 lazy_static! {
     /// Build static reference to the logger that will be accessible from all crates
     pub static ref LOGGER: Logger = {
         let level = Level::Trace;
+
+        // envlogger doesn't allow to set default log level, so this is a workaround
+        if !env::var("RUST_LOG").is_ok() {
+            env::set_var("RUST_LOG", "info");
+        }
+
         // Setup drain for terminal output
         let terminal_decorator = slog_term::TermDecorator::new().build();
         let terminal_drain = slog_term::FullFormat::new(terminal_decorator).build().fuse();
         let terminal_drain = LevelFilter::new(terminal_drain, level).fuse();
+        let terminal_drain = slog_envlogger::new(terminal_drain);
         let terminal_drain = slog_async::Async::new(terminal_drain).build().fuse();
 
         // Setup drain for file output
