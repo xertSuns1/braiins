f02aae42ffe2b32e9dee2daa54c91f5ddceb4204
Libor Vašíček <libor.vasicek@braiins.com>
2019-07-12 20:29:30 +0200

    rurminer: Prepare blocking thread for Block Erupter task
    - spawn blocking thread inside tokio with main task and pass to it work
      solver parameter
    - add sample implementation of asynchronous communication with work
      generator inside blocking thread
    - add to 'utils' module compatibility function 'compat_block_on' which
      run a future to completion on the current thread

--- open/bosminer/src/hal/erupter.rs
+++ open/bosminer/src/hal/erupter.rs
@@ -1,14 +1,48 @@
+use crate::utils::compat_block_on;
 use crate::work;
 
+use tokio_threadpool::blocking;
+
+// use old futures which is compatible with current tokio
+use futures_01::future::poll_fn;
 use futures_locks::Mutex;
 
 use std::sync::Arc;
 
+fn main_task(work_solver: work::Solver) {
+    let (mut generator, _solution_sender) = work_solver.split();
+
+    use crate::misc::LOGGER;
+    use slog::info;
+
+    info!(LOGGER, "Erupter: waiting for work...");
+    let work = compat_block_on(generator.generate()).unwrap();
+    info!(LOGGER, "Erupter: {:?}", work.job);
+}
+
 /// Entry point for running the hardware backend
 pub fn run(
-    _work_solver: work::Solver,
+    work_solver: work::Solver,
     _mining_stats: Arc<Mutex<super::MiningStats>>,
     _shutdown: crate::hal::ShutdownSender,
 ) {
-    // TODO: implement backend
+    // wrap the work solver to Option to overcome FnOnce closure inside FnMut
+    let mut work_solver = Some(work_solver);
+
+    // spawn future in blocking context which guarantees that the task is run in separate thread
+    tokio::spawn(
+        // Because `blocking` returns `Poll`, it is intended to be used from the context of
+        // a `Future` implementation. Since we don't have a complicated requirement, we can use
+        // `poll_fn` in this case.
+        poll_fn(move || {
+            blocking(|| {
+                main_task(
+                    work_solver
+                        .take()
+                        .expect("`tokio_threadpool::blocking` called FnOnce more than once"),
+                )
+            })
+            .map_err(|_| panic!("the threadpool shut down"))
+        }),
+    );
 }
