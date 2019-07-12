bc6058f93b35d7a19b665a4e5a263cc077b57d4f
Libor Vašíček <libor.vasicek@braiins.com>
2019-07-12 20:29:30 +0200

    rurminer: Prepare blocking thread for Block Erupter task
    - spawn blocking thread inside tokio with main task and pass to it work
      solver parameter
    - add sample implementation of asynchronous communication with work
      generator inside blocking thread
    - add to 'utils' module compatibility function 'compat_block_on' which
      run a future to completion on the current thread

--- open/bosminer/src/utils.rs
+++ open/bosminer/src/utils.rs
@@ -1,4 +1,8 @@
-use std::future::Future;
+use tokio::prelude::*;
+
+use futures::{FutureExt, TryFutureExt};
+
+use std::future::Future as StdFuture;
 use wire::utils::CompatFix;
 
 /// Start Tokio runtime
@@ -10,7 +14,7 @@ use wire::utils::CompatFix;
 /// Tokio internals.
 pub fn run_async_main_exits<F>(future: F)
 where
-    F: Future<Output = ()> + Send + 'static,
+    F: StdFuture<Output = ()> + Send + 'static,
 {
     use tokio::runtime::Runtime;
 
@@ -18,4 +22,16 @@ where
     runtime
         .block_on(future.compat_fix())
         .expect("main task can't return error");
+}
+
+/// Run a future to completion on the current thread.
+///
+/// This function will block the caller until the given future has completed.
+/// This implementation is compatible with tokio.
+pub fn compat_block_on<F: StdFuture + Send>(f: F) -> F::Output {
+    f.unit_error()
+        .boxed()
+        .compat()
+        .wait()
+        .expect("future in `compat_block_on` returned error")
 }
