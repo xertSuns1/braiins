76ce693da35bfd6d6ec7faf42378d63cf615a18a
Libor Vašíček <libor.vasicek@braiins.com>
2019-07-23 19:05:05 +0200

    rurminer: Add check for backend selection
    - add check that at least one backend is selected
    - add check for backend 'antminer_s9' that supported target is set
    - move all backend related errors to submodule which is included only
      if some backend has been selected (it prevents printing additional
      compiler related errors)

--- open/bosminer/src/lib.rs
+++ open/bosminer/src/lib.rs
@@ -11,3 +11,21 @@ pub mod work;
 
 #[cfg(test)]
 pub mod test_utils;
+
+#[cfg(not(feature = "backend_selected"))]
+compile_error!(
+    "Backend \"antminer_s9\" or \"erupter\" must be selected with parameter '--features'."
+);
+
+#[cfg(all(
+    feature = "antminer_s9",
+    not(all(
+        target_arch = "arm",
+        target_vendor = "unknown",
+        target_os = "linux",
+        target_env = "musl"
+    ))
+))]
+compile_error!(
+    "Target \"arm-unknown-linux-musleabi\" for \"antminer_s9\" must be selected with parameter '--target'."
+);
