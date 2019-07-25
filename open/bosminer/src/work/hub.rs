fe23a537ab4a1d5782c5fc5530e42f6b55134fd9
Libor Vašíček <libor.vasicek@braiins.com>
2019-07-25 15:08:24 +0200

    rurminer: Refactor work hub constructors
    - add method 'new' to 'JobSolver' and move 'build_solvers' functionality
      to it
    - pass 'EngineReceiver' instead of 'work::Generator' to constructor of
      'work::Solver'
    - fix stale comment

--- open/bosminer/src/work/hub.rs
+++ open/bosminer/src/work/hub.rs
@@ -18,19 +18,18 @@ pub struct Hub;
 impl Hub {
     /// Create Solvers for frontend (pool) and backend (HW accelerator)
     pub fn build_solvers() -> (JobSolver, work::Solver) {
-        let current_target = Arc::new(RwLock::new(uint::U256::from_big_endian(
-            &btc::DIFFICULTY_1_TARGET_BYTES,
-        )));
         let (engine_sender, engine_receiver) = engine_channel();
         let (solution_queue_tx, solution_queue_rx) = mpsc::unbounded();
         (
-            JobSolver {
-                job_sender: hub::JobSender::new(engine_sender, current_target.clone()),
-                solution_receiver: hub::JobSolutionReceiver::new(solution_queue_rx, current_target),
-            },
-            work::Solver::new(Generator::new(engine_receiver), solution_queue_tx),
+            JobSolver::new(engine_sender, solution_queue_rx),
+            work::Solver::new(engine_receiver, solution_queue_tx),
         )
     }
+}
+
+/// Helper function for creating target difficulty suitable for sharing
+pub fn create_shared_target(value: uint::U256) -> Arc<RwLock<uint::U256>> {
+    Arc::new(RwLock::new(value))
 }
 
 /// Compound object for job submission and solution reception intended to be passed to
@@ -41,6 +40,18 @@ pub struct JobSolver {
 }
 
 impl JobSolver {
+    pub fn new(
+        engine_sender: EngineSender,
+        solution_queue_rx: mpsc::UnboundedReceiver<hal::UniqueMiningWorkSolution>,
+    ) -> Self {
+        let current_target =
+            create_shared_target(uint::U256::from_big_endian(&btc::DIFFICULTY_1_TARGET_BYTES));
+        Self {
+            job_sender: hub::JobSender::new(engine_sender, current_target.clone()),
+            solution_receiver: hub::JobSolutionReceiver::new(solution_queue_rx, current_target),
+        }
+    }
+
     pub fn send_job(&mut self, job: Arc<dyn hal::BitcoinJob>) {
         self.job_sender.send(job)
     }
