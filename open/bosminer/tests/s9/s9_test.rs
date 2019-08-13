5d3f5f9507c5f9318322a32bfc7598e45bebfcbb
Jakub Horak <jakub.horak@braiins.com>
2019-08-13 15:45:42 +0200

    rurminer: s9: Fix s9_test
    - make new engine for sending pieces of work (OneWorkEngine)
    - send work to hashchain and count replies

--- open/bosminer/tests/s9/s9_test.rs
+++ open/bosminer/tests/s9/s9_test.rs
@@ -2,11 +2,15 @@ use rminer::hal::{self, s9::null_work};
 use rminer::utils;
 use rminer::work;
 
+use rminer::misc::LOGGER;
+use slog::info;
+
 use std::default::Default;
+use std::fmt;
 use std::time::{Duration, Instant};
 
 use futures_locks::Mutex;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex as StdMutex, MutexGuard as StdMutexGuard};
 
 use futures::channel::mpsc;
 use tokio::await;
@@ -30,6 +34,61 @@ pub fn prepare_test_work() -> hal::MiningWork {
     hal::MiningWork::new(job, vec![mid], time)
 }
 
+struct OneWorkEngineInner {
+    work: Option<hal::MiningWork>,
+}
+
+impl fmt::Debug for OneWorkEngineInner {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OneWorkEngineInner")
+    }
+}
+
+impl OneWorkEngineInner {
+    fn is_exhausted(&self) -> bool {
+        self.work.is_none()
+    }
+
+    fn next_work(&mut self) -> hal::WorkLoop<hal::MiningWork> {
+        match self.work.take() {
+            Some(work) => hal::WorkLoop::Break(work),
+            None => return hal::WorkLoop::Exhausted,
+        }
+    }
+}
+
+pub struct OneWorkEngine {
+    inner: StdMutex<OneWorkEngineInner>,
+}
+
+impl fmt::Debug for OneWorkEngine {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "OneWorkEngine")
+    }
+}
+
+impl OneWorkEngine {
+    pub fn new(work: hal::MiningWork) -> Self {
+        Self {
+            inner: StdMutex::new(OneWorkEngineInner { work: Some(work) }),
+        }
+    }
+
+    fn lock_inner(&self) -> StdMutexGuard<OneWorkEngineInner> {
+        self.inner.lock().expect("cannot lock test work engine")
+    }
+}
+
+impl hal::WorkEngine for OneWorkEngine {
+    fn is_exhausted(&self) -> bool {
+        self.lock_inner().is_exhausted()
+    }
+
+    fn next_work(&self) -> hal::WorkLoop<hal::MiningWork> {
+        self.lock_inner().next_work()
+    }
+}
+
 /// Count replies (even duplicate ones) and erase counters
 pub async fn check_solution_count(mining_stats: Arc<Mutex<hal::MiningStats>>) -> u64 {
     let mut stats = await!(mining_stats.lock()).expect("lock mining stats");
@@ -41,20 +100,19 @@ pub async fn check_solution_count(mining_stats: Arc<Mutex<hal::MiningStats>>) ->
 
 /// Receive workloads and count replies
 async fn send_and_receive_test_workloads<'a>(
-    tx_work: &'a mpsc::UnboundedSender<hal::MiningWork>,
+    engine_sender: &'a mut work::EngineSender,
+    _solution_receiver: &'a mpsc::UnboundedReceiver<hal::UniqueMiningWorkSolution>,
     mining_stats: Arc<Mutex<hal::MiningStats>>,
     n_send: usize,
     expected_solution_count: usize,
 ) {
-    println!(
-        "Sending {} work items and trying to receive {} work items",
-        n_send, expected_solution_count,
+    info!(
+        LOGGER,
+        "Sending {} work items and trying to receive {} solutions", n_send, expected_solution_count,
     );
     // Put in some tasks
     for _ in 0..n_send {
-        tx_work
-            .unbounded_send(prepare_test_work())
-            .expect("send failed");
+        engine_sender.broadcast(Arc::new(OneWorkEngine::new(prepare_test_work())));
 
         // wait until the work is physically sent out it takes around 5 ms for the FPGA IP core
         // to send out the work @ 115.2 kBaud
@@ -62,6 +120,20 @@ async fn send_and_receive_test_workloads<'a>(
     }
     let received_solution_count = await!(check_solution_count(mining_stats.clone())) as usize;
     assert_eq!(expected_solution_count, received_solution_count);
+}
+
+fn build_solvers() -> (
+    work::EngineSender,
+    mpsc::UnboundedReceiver<hal::UniqueMiningWorkSolution>,
+    work::Solver,
+) {
+    let (engine_sender, engine_receiver) = work::engine_channel();
+    let (solution_queue_tx, solution_queue_rx) = mpsc::unbounded();
+    (
+        engine_sender,
+        solution_queue_rx,
+        work::Solver::new(engine_receiver, solution_queue_tx),
+    )
 }
 
 /// Verifies work generation for a hash chain
@@ -78,11 +150,8 @@ fn test_work_generation() {
     let (shutdown_sender, shutdown_receiver) = hal::Shutdown::new().split();
 
     utils::run_async_main_exits(async move {
-        // Create workhub
-        let (_, work_solver) = work::Hub::build_solvers();
-
-        // Create queue with which to inject work into miner
-        let (tx_work, _rx_work) = mpsc::unbounded();
+        // Create solver and channels to send/receive work
+        let (mut engine_sender, solution_queue_rx, work_solver) = build_solvers();
 
         // Create mining stats
         let mining_stats = Arc::new(Mutex::new(hal::MiningStats::new()));
@@ -98,7 +167,8 @@ fn test_work_generation() {
         // the first 3 work loads don't produce any solutions, these are merely to initialize the input
         // queue of each hashing chip
         await!(send_and_receive_test_workloads(
-            &tx_work,
+            &mut engine_sender,
+            &solution_queue_rx,
             mining_stats.clone(),
             3,
             0
@@ -111,7 +181,8 @@ fn test_work_generation() {
         let expected_solution_count = more_work_count * h_chain_guard.get_chip_count();
         drop(h_chain_guard);
         await!(send_and_receive_test_workloads(
-            &tx_work,
+            &mut engine_sender,
+            &solution_queue_rx,
             mining_stats.clone(),
             more_work_count,
             expected_solution_count,
