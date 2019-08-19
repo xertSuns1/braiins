62013f7771b203c6683e343ad92dd1ec4f33fdc6
Jakub Horak <jakub.horak@braiins.com>
2019-08-19 16:49:35 +0200

    rurminer: s9: Increase midstate count to 4
    - this effectively enables AsicBoost

--- open/bosminer/src/hal/s9/config.rs
+++ open/bosminer/src/hal/s9/config.rs
@@ -1,7 +1,7 @@
 /// This module holds configuration for S9 miner until better solution (registry of sorts?) is implemented.
 
 /// Number of midstates
-pub const MIDSTATE_COUNT: usize = 1;
+pub const MIDSTATE_COUNT: usize = 4;
 
 /// Index of hashboard that is to be instantiated
 pub const S9_HASHBOARD_INDEX: usize = 8;
