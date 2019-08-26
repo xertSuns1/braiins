05e9a7e42e1d79b7ae672ed4965e31a81a8c0abe
Jakub Horak <jakub.horak@braiins.com>
2019-08-26 14:09:14 +0200

    rurminer: s9: Bump ASIC difficulty to 256
    Bitmain bm1387 chip compares header hash with ASIC target using
    wrong bit order (on byte level), so when difficulty is set to of
    `2^n` where `n` is not divisible by 8, we may get results with
    difficulty not high enough (while the high-enough may get filtered
    out).

--- open/bosminer/src/hal/s9/config.rs
+++ open/bosminer/src/hal/s9/config.rs
@@ -8,7 +8,7 @@ pub const MIDSTATE_COUNT: usize = 4;
 pub const S9_HASHBOARD_INDEX: usize = 8;
 
 /// Default ASIC difficulty
-pub const ASIC_DIFFICULTY: usize = 64;
+pub const ASIC_DIFFICULTY: usize = 256;
 
 /// Maximum time it takes to compute one job under normal circumstances
 pub const JOB_TIMEOUT: Duration = Duration::from_secs(5);
