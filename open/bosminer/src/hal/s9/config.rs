e6ab4143749dca5357259040308ea06df6390703
Jakub Horak <jakub.horak@braiins.com>
2019-09-02 14:48:45 +0200

    rurminer: Add `JOB_TIMEOUT` config option
    This is mostly for debugging.

--- open/bosminer/src/hal/s9/config.rs
+++ open/bosminer/src/hal/s9/config.rs
@@ -1,4 +1,5 @@
 /// This module holds configuration for S9 miner until better solution (registry of sorts?) is implemented.
+use std::time::Duration;
 
 /// Number of midstates
 pub const MIDSTATE_COUNT: usize = 4;
@@ -8,3 +9,6 @@ pub const S9_HASHBOARD_INDEX: usize = 8;
 
 /// Default ASIC difficulty
 pub const ASIC_DIFFICULTY: usize = 64;
+
+/// Maximum time it takes to compute one job under normal circumstances
+pub const JOB_TIMEOUT: Duration = Duration::from_secs(5);
