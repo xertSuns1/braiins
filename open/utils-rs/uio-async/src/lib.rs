698758b5023ac2e856ba546ff876c01baf6c061f
Vojtech Kral <vojtech.kral@braiins.com>
2019-10-09 16:01:55 +0200

    uio-async: Update to Tokio 0.2  #2582

--- open/utils-rs/uio-async/src/lib.rs
+++ open/utils-rs/uio-async/src/lib.rs
@@ -1,11 +1,3 @@
-#![feature(await_macro, async_await)]
-
-extern crate fs2;
-extern crate futures;
-extern crate libc;
-extern crate nix;
-extern crate timeout_readwrite;
-
 #[cfg(target_os = "linux")]
 mod linux;
 
