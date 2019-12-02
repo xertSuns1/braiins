00da272df9e342dde67d63f15d3f14fba8795369
Jakub Horak <jakub.horak@braiins.com>
2019-12-02 12:42:03 +0100

    bosminer: s9: Refactor firmware loader
    - move the firmware loading code to `power::Control`
    - leave the test in `power::firmware`
    - move reset/start info messages to respective methods

--- open/bosminer/bosminer-am1-s9/src/power/firmware.rs
+++ open/bosminer/bosminer-am1-s9/src/power/firmware.rs
@@ -23,7 +23,7 @@
 //! PIC firmware loader
 
 use crate::error::{self, ErrorKind};
-use crate::power::{self, PicAddress, PicWords};
+use crate::power::{PicAddress, PicWords};
 use failure::ResultExt;
 
 use std::convert::AsRef;
@@ -48,22 +48,14 @@ const PROGRAM_LOAD_END_ADDRESS: PicAddress = PicAddress(0x0f7f);
 /// Program to be loaded to PIC of voltage controller
 #[derive(Clone)]
 pub struct PicProgram {
-    load_addr: PicAddress,
-    prog_size: PicWords,
-    bytes: Vec<u8>,
+    pub load_addr: PicAddress,
+    pub prog_size: PicWords,
+    pub bytes: Vec<u8>,
 }
 
 impl PicProgram {
     /// Construct loadable PIC program from bytes
     pub fn from_bytes(bytes: Vec<u8>) -> error::Result<Self> {
-        if bytes.len() % power::Control::FLASH_XFER_BLOCK_SIZE_BYTES != 0 {
-            // This is irrelevant now (we check size), but otherwise it's required because
-            // the self-programmer can only load whole blocks
-            Err(ErrorKind::Power(format!(
-                "PIC program size not divisible by {}",
-                power::Control::FLASH_XFER_BLOCK_SIZE_BYTES
-            )))?
-        }
         let prog_size = PROGRAM_LOAD_ADDRESS.distance_to(PROGRAM_LOAD_END_ADDRESS);
         if bytes.len() != prog_size.to_bytes() {
             Err(ErrorKind::Power(format!(
@@ -98,28 +90,12 @@ impl PicProgram {
         }
         Self::from_bytes(bytes)
     }
-
-    /// Load PIC program to voltage controller
-    pub async fn program_pic(&self, voltage_ctrl: &power::Control) -> error::Result<()> {
-        voltage_ctrl.reset().await?;
-        voltage_ctrl
-            .erase_flash(self.load_addr, self.prog_size)
-            .await?;
-        voltage_ctrl
-            .write_flash(self.load_addr, &self.bytes[..])
-            .await?;
-        if voltage_ctrl.get_flash_pointer().await? != self.load_addr.offset(self.prog_size) {
-            Err(ErrorKind::Power(
-                "flash pointer ended at invalid address".into(),
-            ))?
-        }
-        Ok(())
-    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::power;
     use ii_async_compat::tokio;
     use ii_logging::macros::*;
     use std::sync::Arc;
@@ -154,33 +130,33 @@ mod test {
     async fn test_pic_reload_program() {
         let voltage_ctrl_backend = Arc::new(power::I2cBackend::new(0));
         let voltage_ctrl = power::Control::new(voltage_ctrl_backend, 8);
-        let program = power::firmware::PicProgram::read(power::PIC_PROGRAM_PATH)
+        let good_program = power::firmware::PicProgram::read(power::PIC_PROGRAM_PATH)
             .expect("program read failed");
 
         // Load garbage program
         info!("Loading bad program");
-        let mut bad_program = program.clone();
+        let mut bad_program = good_program.clone();
         // PIC has 14-bit flash, so avoid setting highest two bits of each word (which is BE u16)
         bad_program.bytes = vec![0x3f, 0xff, 0x2a, 0xaa, 0x15, 0x55]
             .into_iter()
             .cycle()
             .take(bad_program.bytes.len())
             .collect::<Vec<u8>>();
-        bad_program
-            .program_pic(&voltage_ctrl)
+        voltage_ctrl
+            .program_pic(&bad_program)
             .await
-            .expect("bad_program load failed");
+            .expect("bad program load failed");
         verify_program(&voltage_ctrl, &bad_program)
             .await
             .expect("PIC dump failed");
 
         // Load good program
         info!("Loading good program");
-        program
-            .program_pic(&voltage_ctrl)
+        voltage_ctrl
+            .program_pic(&good_program)
             .await
-            .expect("program load failed");
-        verify_program(&voltage_ctrl, &program)
+            .expect("good program load failed");
+        verify_program(&voltage_ctrl, &good_program)
             .await
             .expect("PIC dump failed");
 
