0d045328bd1f3046661d9a41de6b42e9b650d684
Hynek Fabian <hynek.fabian@braiins.cz>
2019-12-12 15:30:17 +0100

    bosminer: Faster runner upload
    - add an option to run command (ie. strip) before copying to remote
    - add an option to compress test for transfer
    - stop if lock acquiring failed

--- open/bosminer/scripts/runner.py
+++ open/bosminer/scripts/runner.py
@@ -40,6 +40,7 @@ import re
 import argparse
 import toml
 import subprocess
+import shutil
 
 CONFIG_FILE = 'Test.toml'
 DEFAULT_USER = 'root'
@@ -50,20 +51,26 @@ def main():
     parser = argparse.ArgumentParser(__doc__)
 
     parser.add_argument('test',
-                        help='path to executable with the test')
+                        help='Path to executable with the test.')
+    parser.add_argument('--apply', metavar='COMMAND',
+                        help="Run command on input file. Path to test will replace string '{}' or be appended.")
+    parser.add_argument('--compress', action='store_true',
+                        help='Compress file before transfer.')
     arg_hostname = parser.add_argument('--hostname',
-                        help='ip address or hostname of remote bOS device with ssh server')
-    parser.add_argument('--keep', action="store_true",
-                        help='keep remote file')
+                        help='Ip address or hostname of remote bOS device with ssh server.')
+    parser.add_argument('--keep', action='store_true',
+                        help='Keep remote file.')
     parser.add_argument('--path', dest='host_path', default='/tmp', metavar='PATH',
-                        help='remote path')
+                        help='Target path on remote host.')
     parser.add_argument('--user',
-                        help='remote login user')
-    parser.add_argument('--verbose', action="store_true",
-                        help='log commands')
+                        help='Remote login user.')
+    parser.add_argument('--verbose', action='store_true',
+                        help='Log commands to stdout.')
 
     # parse command line arguments
     (args, extra_args) = parser.parse_known_args()
+
+    runner = run_verbose if args.verbose else run_silent
 
     cfg_user = None
     cfg_hostname = None
@@ -88,75 +95,115 @@ def main():
         parser.error("Missing remote hostname which can be specified by '{}' argument or in '{}'"
               .format(arg_hostname.option_strings[0], CONFIG_FILE))
 
-    test = args.test
-    test_name = os.path.basename(test)
-
+    test_path = args.test
+    test_name = os.path.basename(args.test)
     # stuff resembling cargo tests (file ends with a dash followed by sixteen hex digits)
     # is automagically endowed with param to enforce single thread to ensure exclusive access to hw
-    remote_argv = ['--test-threads', '1'] if re.match('.+-[0-9a-f]{16}$', test) else []
+    remote_argv = ['--test-threads', '1'] if re.match('.+-[0-9a-f]{16}$', args.test) else []
     remote_argv += extra_args
-    remote_test = os.path.join(args.host_path, os.path.basename(args.test))
+    remote_test = os.path.join(args.host_path, test_name)
     common_args = ['-o', 'StrictHostKeyChecking=no']
+
+    if args.apply:
+        test_path = shutil.copy(args.test, '/tmp')
+        deleter = Deleter(test_path)
+        command = args.apply.replace('{}', test_path) if '{}' in args.apply else args.apply + ' ' + test_path
+        runner(
+            command,
+            shell=True,
+            check=True,
+        )
+
+    if args.compress:
+        runner(
+            'gzip',
+            '--keep',
+            '--force',
+            test_path
+        )
+        compression_suffix = '.gz'
+    else:
+        compression_suffix = ''
+
 
     if not args.verbose:
         # suppress message 'Connection to XYZ closed.'
         common_args.append('-q')
 
-    # take a lock, in case multiple runners meet.
-    # note this is openwrt specific, and unlike flock have to be explicitely unlocked
-    run_lock = run(
+    # create target dir if not exist and acquire a lock, in case multiple runners meet.
+    # note this locking is openwrt specific, and unlike flock have to be explicitely unlocked
+    run_lock = runner(
         'ssh',
         *common_args,
         '-l', user,
         hostname,
-        'lock /tmp/testrunner && mkdir -p ' + args.host_path,
-        verbose=args.verbose,
-        check=False,
+        'mkdir -p ' + args.host_path + ' && lock /tmp/testrunner',
+        check=True,
     )
 
     try:
-        cpy_ret = run(
+        if args.verbose:
+            print('copying %d bytes' % os.path.getsize(test_path))
+        cpy_ret = runner(
             'scp',
             '-q',   # no progressbar
             '-C',   # enable compression
             *common_args,
-            args.test,
+            test_path + compression_suffix,
             '{}@{}:{}'.format(user, hostname, args.host_path),
-            verbose=args.verbose,
             check=True,
         )
 
-        run_ret = run(
+        if args.compress:
+            run_dec = runner(
+                'ssh',
+                *common_args,
+                '-l', user,
+                hostname,
+                'gunzip ' + remote_test + compression_suffix,
+                check=True,
+            )
+
+        run_ret = runner(
             'ssh',
             *common_args,
             '-t',   # force pty
             '-l', user,
             hostname,
             remote_test + ' ' + ' '.join(remote_argv),
-            verbose=args.verbose,
             check=False,
         )
 
     finally:
         # clean up code and lock
-        clean_ret = run(
+        clean_ret = runner(
             'ssh',
             *common_args,
             '-l', user,
             hostname,
             ('rm -f ' + remote_test + ' ; ' if not args.keep else '') + 'lock -u /tmp/testrunner',
-            verbose=args.verbose,
             check=True,
         )
 
     sys.exit(run_ret.returncode)
 
 
-def run(*args, verbose=False, **kargs):
-    """wrap subprocess.run to log executed commands on stdout"""
-    if verbose:
-        print(' '.join(args), flush=True)
+def run_silent(*args, **kargs):
+    """wrapper over subprocess for nicer call format"""
     return subprocess.run(args, **kargs)
+
+
+def run_verbose(*args, **kargs):
+    """wrap subprocess.run to log executed commands on stdout"""
+    print(' '.join(args), flush=True)
+    return run_silent(*args, **kargs)
+
+class Deleter:
+    def __init__(self, path):
+        self.path = path
+
+    def __del__(self):
+        os.unlink(self.path)
 
 
 if __name__ == "__main__":
