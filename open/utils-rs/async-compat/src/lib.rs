9a71c093c0adb4e27e1164df948a2ee587c58685
Vojtech Kral <vojtech.kral@braiins.com>
2020-02-07 17:51:40 +0100

    async-compat: HaltHandle: Refactor
    - Add HaltHandle::ready() to avoid possible race conditions when spawning tasks
    - Add/Improve doc comments
    - Add a race condition test

--- open/utils-rs/async-compat/src/lib.rs
+++ open/utils-rs/async-compat/src/lib.rs
@@ -125,20 +125,34 @@ macro_rules! join {
 }
 
 /// Internal, used to signal termination via `trigger`
-/// and notify `Joins` when that happens.
+/// and notify `Tasks` when that happens.
 #[derive(Debug)]
 struct Halt {
     trigger: Trigger,
     notify_tx: oneshot::Sender<()>,
 }
 
+/// Internal, used in the `Tasks` channel,
+/// contains either a join handle of a task
+/// that was spawned or a ready notification which
+/// indicates to the `join()` function that all necessary tasks
+/// were spawned.
+///
+/// `spawn()` uses this to send a spawned task's handle,
+/// `ready()` to send a Ready notification.
+#[derive(Debug)]
+enum TaskMsg {
+    Task(JoinHandle<()>),
+    Ready,
+}
+
 /// Internal, used in `HaltHandle::join()`
 /// to wait on signal from `halt()`
 /// and then collect halting tasks' join handles.
 #[derive(Debug)]
-struct Joins {
-    joins_rx: mpsc::UnboundedReceiver<JoinHandle<()>>,
-    notify_rx: oneshot::Receiver<()>,
+struct Tasks {
+    tasks_rx: mpsc::UnboundedReceiver<TaskMsg>,
+    halt_notify_rx: oneshot::Receiver<()>,
 }
 
 /// Error type returned by `HaltHandle::join()`.
@@ -179,44 +193,58 @@ impl StdError for HaltError {
     }
 }
 
-/// A handle with which tasks can be spawned
-/// that can be then signaled to halt.
+/// A handle with which tasks can be spawned and then halted.
+///
+/// # Usage
+/// 1. Create a `HaltHandle` with `HaltHandle::new()` or `HaltHandle::arc()`
+/// (use the latter if you want to share it between tasks or use `halt_on_ctrlc()`).
+/// 2. Spawn any number of tasks using the `spawn()` method.
+/// 3. When all relevant `spawn()` calls were made, use the `ready()` method
+///    to tell the `HaltHandle` that all tasks were spawned.
+/// 4. Use `halt()` to tell the spawned tasks that they should stop.
+///    You can also use `halt_on_ctrlc()`, which will setup a
+///    handler that calls `halt()` on `SIGINT`.
+/// 5. Use `join()` to wait on the tasks to stop (a timeout may be used).
+///
+/// Note that `halt()` or `halt_on_ctrlc()` doesn't necessarily need to be called
+/// after `ready()`. These can be called pretty much anytime and it won't cause
+/// a race condition as long as `ready()` is called in the right moment.
 #[derive(Debug)]
 pub struct HaltHandle {
     /// `stream-cancels` tripwire that is cloned into
     /// 'child' tasks when they are started with this handle.
     tripwire: Tripwire,
-    /// Used to trigger the tripwire and then signals to `joins`.
+    /// Used to trigger the tripwire and then notifies `tasks`.
     halt: Mutex<Option<Halt>>,
-    /// Child tasks join handles are sent here to be awaited in `join()`.
-    joins_tx: mpsc::UnboundedSender<JoinHandle<()>>,
+    /// Spawned task handles as well as a ready notification are sent here, see `JoinsMsg`
+    tasks_tx: mpsc::UnboundedSender<TaskMsg>,
     /// Used to receive notification from `halt` and the task handles.
-    joins: Mutex<Option<Joins>>,
+    tasks: Mutex<Option<Tasks>>,
     /// A flag whether we've already spawned a ctrlc tasks;
     /// this can only be done once.
     ctrlc_task_spawned: AtomicBool,
 }
 
 impl HaltHandle {
-    /// Constructor
+    /// Create a new `HaltHandle`
     pub fn new() -> Self {
         let (trigger, tripwire) = Tripwire::new();
-        let (notify_tx, notify_rx) = oneshot::channel();
-        let (joins_tx, joins_rx) = mpsc::unbounded_channel();
+        let (notify_tx, halt_notify_rx) = oneshot::channel();
+        let (tasks_tx, tasks_rx) = mpsc::unbounded_channel();
 
         Self {
             tripwire,
             halt: Mutex::new(Some(Halt { trigger, notify_tx })),
-            joins_tx,
-            joins: Mutex::new(Some(Joins {
-                joins_rx,
-                notify_rx,
+            tasks_tx,
+            tasks: Mutex::new(Some(Tasks {
+                tasks_rx,
+                halt_notify_rx,
             })),
             ctrlc_task_spawned: AtomicBool::new(false),
         }
     }
 
-    /// Create a handle and wrap in `Arc` for sharing between tasks.
+    /// Create a `HaltHandle` and wrap it in `Arc` for sharing between tasks
     pub fn arc() -> Arc<Self> {
         Arc::new(Self::new())
     }
@@ -224,7 +252,7 @@ impl HaltHandle {
     /// Spawn a new task. `f` is a function that takes
     /// a `Tripwire` and returns a `Future` to be spawned.
     /// `Tripwire` can be passed to `StreamExt::take_until`
-    /// to make the stream stop generating items when
+    /// to make a stream stop generating items when
     /// `halt()` is called on the `HaltHandle`.
     pub fn spawn<FT, FN>(&self, f: FN)
     where
@@ -233,16 +261,20 @@ impl HaltHandle {
     {
         let ft = f(self.tripwire.clone());
         let task = tokio::spawn(ft);
-        // Send the task handle so that join() can join on it later
-        self.add_task(task);
-    }
 
-    pub(crate) fn add_task(&self, task: JoinHandle<()>) {
-        // Add the task join handle to joins_tx (used by join()).
+        // Add the task join handle to tasks_tx (used by join()).
         // Errors are ignored here - send() on an unbounded channel
         // only fails if the receiver is dropped, and in that case
         // we don't care that the send() failed...
-        let _ = self.joins_tx.send(task);
+        let _ = self.tasks_tx.send(TaskMsg::Task(task));
+    }
+
+    /// Tells the handle that all tasks were spawned
+    pub fn ready(&self) {
+        // Send a Ready message. join() uses this to tell
+        // that enough join handles were collected.
+        // Error is ignored here for the same reason as in spawn().
+        let _ = self.tasks_tx.send(TaskMsg::Ready);
     }
 
     /// Tell the handle to halt all the associated tasks.
@@ -279,7 +311,7 @@ impl HaltHandle {
         }
     }
 
-    /// Wait for all associated tasks to finish once `halt()` is called.
+    /// Wait for all associated tasks to finish once `ready()` and `halt()` are called.
     ///
     /// An optional `timeout` may be provided, this is the maximum time
     /// to wait **after** `halt()` has been called.
@@ -287,24 +319,32 @@ impl HaltHandle {
     /// Returns `Ok(())` when tasks are collected succesfully, or a `HaltError::Timeout`
     /// if tasks tasks didn't stop in time, or a `HaltError::Join` when a task panics.
     /// If multiple tasks panic, the first join error encountered is returned.
+    ///
+    /// # Panics
+    /// `join()` panics if you call it multiple times. It must only be called once.
     pub async fn join(&self, timeout: Option<Duration>) -> Result<(), HaltError> {
-        let mut joins = self
-            .joins
+        let mut tasks = self
+            .tasks
             .lock()
             .unwrap()
             .take()
             .expect("HaltHandle: join() called multiple times");
-        let _ = joins.notify_rx.await;
+        let _ = tasks.halt_notify_rx.await;
 
         // Collect join handles. Join handles are added to the
-        // joins channel by Self::spawn(). Here we take out all the
-        // join handles that are in the channel right now.
+        // tasks channel by Self::spawn(). After the user decides all
+        // relevant tasks were added, they call ready().
+        // ready() pushes a ready message, TaskMsg::Ready, to this channel.
+        // Here we collect all the task join handles until we reach the ready message.
         let mut handles = vec![];
-        while let Ok(handle) = joins.joins_rx.try_recv() {
-            handles.push(handle);
+        while let Some(task_msg) = tasks.tasks_rx.next().await {
+            match task_msg {
+                TaskMsg::Task(handle) => handles.push(handle),
+                TaskMsg::Ready => break,
+            }
         }
 
-        // Join all the tasks, wait for them to finalize
+        // Join all the spawned tasks, wait for them to finalize
         let ft = future::join_all(handles.drain(..));
         // If there's a timeout, only wait so much
         let mut res = if let Some(timeout) = timeout {
@@ -328,8 +368,8 @@ mod test {
     use super::prelude::*;
     use super::*;
 
-    use std::sync::Arc;
     use std::sync::atomic::{AtomicUsize, Ordering};
+    use std::sync::Arc;
 
     use tokio::{stream, time};
 
@@ -357,7 +397,8 @@ mod test {
             });
         }
 
-        // Signal halt and join tasks
+        // Signal ready, halt, and join tasks
+        handle.ready();
         handle.halt();
         handle.join(None).await.expect("join() failed");
     }
@@ -386,7 +427,52 @@ mod test {
         });
 
         // Join tasks
+        handle.ready();
         handle.join(None).await.expect("join() failed");
+    }
+
+    // Test that spawn() / halt() / join() is not racy when ready()
+    // is used appropriately.
+    #[tokio::test(threaded_scheduler)]
+    async fn test_halthandle_race() {
+        const NUM_TASKS: usize = 10;
+
+        let handle = HaltHandle::arc();
+        let num_cancelled = Arc::new(AtomicUsize::new(0));
+
+        // Signal halt right away, this should be fine
+        handle.halt();
+
+        // Spawn tasks in another task to allow a race
+        {
+            let handle = handle.clone();
+            let num_cancelled = num_cancelled.clone();
+
+            tokio::spawn(async move {
+                // Delay a bit so that join() happens sooner than spawns
+                time::delay_for(Duration::from_millis(100)).await;
+
+                // Spawn a couple of tasks on the handle
+                for _ in 0..NUM_TASKS {
+                    let num_cancelled = num_cancelled.clone();
+                    handle.spawn(|tripwire| {
+                        async move {
+                            forever_stream(tripwire).await;
+                            num_cancelled.fetch_add(1, Ordering::SeqCst);
+                        }
+                    });
+                }
+
+                // Finally, signal that tasks are ready
+                handle.ready();
+            });
+        }
+
+        // Join tasks
+        handle.join(None).await.expect("join() failed");
+
+        let num_cancelled = num_cancelled.load(Ordering::SeqCst);
+        assert_eq!(num_cancelled, NUM_TASKS);
     }
 
     // Test that if cleanup after halt takes too long, handler will return the right error
@@ -403,6 +489,7 @@ mod test {
             }
         });
 
+        handle.ready();
         handle.halt();
         let res = handle.join(Some(Duration::from_millis(100))).await;
 
@@ -427,6 +514,7 @@ mod test {
             }
         });
 
+        handle.ready();
         handle.halt();
         let res = handle.join(Some(Duration::from_millis(100))).await;
 
